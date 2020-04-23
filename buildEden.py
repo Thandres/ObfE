84367a5e2ea9253962791a34fdf77eb2a2e75576
Thandres <thandres@users.noreply.github.com>
2020-04-23 23:13:32 +0200

    implemented combining traversing folders and getting all XML files

--- buildEden.py
+++ buildEden.py
@@ -4,7 +4,24 @@ from edenSources import destination_list, destination_tuples, get_destination_fi
 
 def build():
     destination_folder_dict = get_build_folders()
+    for destination in destination_folder_dict.keys():
+        folders = destination_folder_dict[destination]
+        content = ""
+        for folder in folders:
+            content += get_strings(os.path.join(os.getcwd(),folder))
+        print(destination)
+        print(content)
 
+def get_strings(file_path):
+    content =""
+    for root, _, files in os.walk(file_path):
+        xml_files = [xml for xml in files if xml.endswith(".xml")]
+        for file in xml_files:
+            with open(os.path.join(root,file),"r") as f:
+                xml_lines = f.readlines()
+            xml = "\n" + "".join(xml_lines)
+            content += xml
+    return content
 
 """Returns a map of the destination XML and all folders containing files for that XML"""
 def get_build_folders():
@@ -18,8 +35,9 @@ def get_build_folders():
             with open(file_path, "r") as f:
                 destination = f.readline()
             destination_folder_dict[destination].append(os.path.basename(root))
+            continue
     return destination_folder_dict
 
 
 if __name__ == "__main__":
-    get_build_folders()
+    build()
