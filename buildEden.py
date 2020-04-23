d3bfc1b2fae878af9d06ca9712c91a1c74389622
Thandres <thandres@users.noreply.github.com>
2020-04-23 23:49:07 +0200

    made build accept an output path

--- buildEden.py
+++ buildEden.py
@@ -2,31 +2,35 @@ import os
 from edenSources import destination_list, get_destination_file_name, get_top_tag
 
 
-def build():
+def build(output_folder=os.getcwd()):
     destination_folder_dict = get_build_folders()
     for destination in destination_folder_dict.keys():
         folders = destination_folder_dict[destination]
         content = ""
         for folder in folders:
-            content += get_strings(os.path.join(os.getcwd(),folder))
+            content += get_strings(os.path.join(os.getcwd(), folder))
         if content != "":
             open_tag, close_tag = get_top_tag(destination)
             file_content = open_tag + content + "\n" + close_tag
-            with open(os.path.join(os.getcwd(),destination),"w") as f:
+            with open(os.path.join(output_folder, destination), "w") as f:
                 f.write(file_content)
 
+
 def get_strings(file_path):
-    content =""
+    content = ""
     for root, _, files in os.walk(file_path):
         xml_files = [xml for xml in files if xml.endswith(".xml")]
         for file in xml_files:
-            with open(os.path.join(root,file),"r") as f:
+            with open(os.path.join(root, file), "r") as f:
                 xml_lines = f.readlines()
             xml = "\n" + "".join(xml_lines)
             content += xml
     return content
 
+
 """Returns a map of the destination XML and all folders containing files for that XML"""
+
+
 def get_build_folders():
     destination_folder_dict = {}
     # init empty lists to fill later
